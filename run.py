5529eb12da939f1376f79fb793956161da8c884e
GuyLor <guyll59@gmail.com>
2018-12-25 16:22:12 +0200

    reinforce method have been added

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from scripts.direct_astar import DirectAstar
+from scripts.reinforce import Reinforce
 from scripts.policy_model import Checkpoint
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--env_path', type=str, default='MiniGrid-MultiRoom-N6-v0',
                     help='path of minigrid enviornment, should start with "MiniGrid"')
+parser.add_argument('--method', type=str, default='direct_astar',
+                    help='rl method- direct_astar or reinforce ')
+
 parser.add_argument('--model_dir', type=str, default='saved_models',
                     help='path of pytorch state dicts for saving and loading')
 parser.add_argument('--save_path', type=str, default=None,
@@ -43,22 +47,29 @@ def main():
     cp = Checkpoint(folder_path=args.model_dir,
                     load_path=args.load_path,
                     save_path=args.save_path)
-                    
-    d = DirectAstar(env_path=args.env_path,
-                    chekpoint=cp,
-                    seed=args.seed,
-                    discount=args.discount,
-                    eps_grad=args.eps_grad,
-                    eps_reward=args.eps_reward,
-                    max_steps=args.max_steps,
-                    max_search_time=args.max_search_time,
-                    update_wo_improvement=args.update_wo_improvement,
-                    keep_searching=args.keep_searching)
+    
+    if args.method == 'direct_astar':
+        m = DirectAstar(env_path=args.env_path,
+                        chekpoint=cp,
+                        seed=args.seed,
+                        discount=args.discount,
+                        eps_grad=args.eps_grad,
+                        eps_reward=args.eps_reward,
+                        max_steps=args.max_steps,
+                        max_search_time=args.max_search_time,
+                        update_wo_improvement=args.update_wo_improvement,
+                        keep_searching=args.keep_searching)
+    elif args.method == 'reinforce':
+        m = Reinforce(env_path=args.env_path,
+                        chekpoint=cp,
+                        seed=args.seed,
+                        discount=args.discount,
+                        max_steps=240)
 
     if args.train:
-        d.train(num_episodes=args.episodes,seed=args.seed)
+        m.train(num_episodes=args.episodes,seed=args.seed)
     if args.play:
-        d.play(seed = args.seed,inarow=True,auto=True)
+        m.play(seed = args.seed+9999,inarow=True,auto=True)
 
 if __name__ == '__main__':
     main()
---
+++ scripts/reinforce.py
@@ -0,0 +1,96 @@
+import numpy as np
+import copy
+import time
+import torch
+from torch.distributions import Categorical
+
+import scripts.utils as utils
+from scripts.minigrid_rl import MinigridRL
+
+class Reinforce(MinigridRL):
+    def __init__(self,
+                 env_path,
+                 chekpoint,
+                 seed,
+                 max_steps=240,
+                 discount=0.99):
+        super().__init__(env_path,chekpoint,seed,max_steps,discount)
+        
+        self.max_steps=max_steps
+        self.discount = self.discount
+        self.max_steps = 240
+        self.saved_log_probs = []
+        self.rewards = []
+    
+    def select_action(self,state):
+        #state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.policy([state])
+        m = Categorical(torch.exp(probs))
+        action = m.sample()
+        self.saved_log_probs.append(m.log_prob(action))
+        return action.item()
+    
+    def finish_episode(self):
+        R = 0
+        policy_loss = []
+        rewards = []
+        for r in self.rewards[::-1]:
+            R = r + self.discount * R
+            rewards.insert(0, R)
+        rewards = torch.tensor(rewards)
+        #rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
+        for log_prob, reward in zip(self.saved_log_probs, rewards):
+            policy_loss.append(-log_prob * reward)
+        self.optimizer.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+        self.optimizer.step()
+        del self.rewards[:]
+        del self.saved_log_probs[:]
+
+    def train(self,num_episodes=500,seed = 1234):
+        success = 0
+        rewards = []
+        for episode in range(num_episodes):
+            state = self.env.reset()
+            self.env.seed(seed)
+            print('episode: ',episode)
+            actions=[]
+            done=False
+            while not done:
+                action = self.select_action(state)
+                state, reward, done, _ = self.env.step(action)
+                self.rewards.append(reward)
+                actions.append(action)
+            trajectory_reward,suc = self.run_episode(actions,seed)
+            success += suc
+
+            print ('reinforce reward: {:.3f},success: {}, length: {}'.format(trajectory_reward,suc,len(actions)))
+            rewards.append(trajectory_reward)
+            seed+=1
+            self.finish_episode()
+            if episode % 20 == 1:
+                self.save_checkpoint()
+        self.save_checkpoint()
+        return success,rewards
+    
+    def play(self,sample_opt = True,seed=1234,inarow=True,auto=True):
+
+        def resetEnv(seed):
+            self.env = self.reset()
+            self.env.seed(seed)
+            state = self.env.reset()
+            done=False
+            actions=[]
+            
+            while not done:
+                action = self.select_action(state)
+                state, reward, done, _ = self.env.step(action)
+                actions.append(action)
+            return actions
+        while True:
+            seed+=1
+            actions = resetEnv(seed)
+            super().play(actions,seed,auto=auto)
+            if not inarow:
+                break
